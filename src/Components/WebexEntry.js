7e0ad2092ec48bf6a0964543b1a55068f167faf5
Aabid <aabidagharia@icloud.com>
2021-07-23 11:30:53 +0530

    improve dropdown

--- src/Components/WebexEntry.js
+++ src/Components/WebexEntry.js
@@ -11,30 +11,43 @@ const options = [
         key: 'Computer',
         value: 'Audio: Use Computer audio',
         content: (
-            <Header icon='headphones' content='Audio: Use Computer audio' as='h5'/>
+            <span>
+                <Icon name='headphones'/> Audio: Use Computer audio
+            </span>
+            // <Header icon='headphones' content='Audio: Use Computer audio' as='h5'/>
         ),
         text: (
-            <Header icon='headphones' content='Audio: Use Computer audio' as='h5'/>
+            <span>
+                <Icon name='headphones'/> Audio: Use Computer audio
+            </span>
         )
     },
     {
         key: 'CallIn',
         value: 'Call In',
         content: (
-            <Header icon='call' content='Call In' as='h5'></Header>
+            <span>
+                <Icon name='call'/> Call In
+            </span>
         ),
         text: (
-            <Header icon='call' content='Call In' as='h5'></Header>
+            <span>
+                <Icon name='call'/> Call In
+            </span>
         )
     },
     {
         key: 'Don\'t connect to Audio',
         value: 'Don\'t connect to Audio',
         content: (
-            <Header icon='ban   ' content='Don&#39;t connect to Audio' as='h5'></Header>
+            <span>
+                <Icon name='ban'/> Don't connect to Audio
+            </span>
         ),
         text: (
-            <Header icon='ban   ' content='Don&#39;t connect to Audio' as='h5'></Header>
+            <span>
+                <Icon name='ban'/> Don't connect to Audio
+            </span>
         )
     }
 ]
@@ -44,7 +57,7 @@ const WebexEntry = () => (
         <Grid.Row>
             <Grid.Column textAlign='center' >
                 <Segment>
-                    <Segment textAlign='center' basic padded className="Web-Ex-Height"><Image src='https://infosys.webex.com/webappng/images/avatars/aa.png' size='medium' circular /></Segment>
+                    <Segment textAlign='center' basic padded className="Web-Ex-Height"><Image src='https://infosys.webex.com/webappng/images/avatars/a.png' size='medium' circular /></Segment>
                     <Segment basic>
                         <Grid>
                             <Grid.Row columns={3} stretched verticalAlign='middle'>
@@ -52,15 +65,15 @@ const WebexEntry = () => (
 
                                 </Grid.Column>
                                 <Grid.Column>
-                                    <Segment basic> 
-                                        <Button circular fluid>
-                                            <Dropdown
+                                    {/* <Segment basic>  */}
+                                        <Button circular size='medium' >
+                                            <Dropdown  
                                                 inline
                                                 options = {options}
                                                 defaultValue = {options[1].value}
                                             />
                                         </Button>
-                                    </Segment>
+                                    {/* </Segment> */}
                                 </Grid.Column>
                                 <Grid.Column>
                                     <Segment textAlign="center" basic><a href='www.google.com'><Icon name='setting'/>Test Speaker and Microphone</a></Segment>
@@ -73,9 +86,9 @@ const WebexEntry = () => (
         </Grid.Row>
         <Grid.Row>
             <Grid.Column width={10} textAlign='center'>
-                <Button circular size='large'><Icon name='mute' color='red'/>Unmute</Button>
-                <Button circular size='large'><Icon name='video' color='red'/>Start Video</Button>
-                <Button circular size='large' color='green'>Join Meeting</Button>
+                <Button className = "White-Button" circular size='medium' ><Icon name='mute' color='red'/>Unmute</Button>
+                <Button className="White-Button" circular size='medium'><Icon name='video' color='red'/>Start Video</Button>
+                <Button circular size='medium' color='green'>Join Meeting</Button>
             </Grid.Column>
         </Grid.Row>
         
